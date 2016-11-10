88816f1d02dbff40b45aefa46c9f763d2732badd
MrValera <ElternJack@yandex.ua>
2016-11-11 00:27:02 +0200

    Create script1.js

---
+++ Other-Code/MyProject4/js/script1.js
@@ -0,0 +1,12 @@
+
+$(function(){
+  $('.view-source .hide').hide();
+  $a = $('.view-source a');
+  $a.on('click', function(event) {
+    event.preventDefault();
+    $a.not(this).next().slideUp(500);
+    $(this).next().slideToggle(500);
+  });
+});
+
+
