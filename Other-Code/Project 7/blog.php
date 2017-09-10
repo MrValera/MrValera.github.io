492813d9c2f8bb86f1f0fae8deb1d3ba1ed306bf
MrValera <ElternJack@yandex.ua>
2017-09-10 19:54:34 +0300

    Add files via upload

---
+++ Other-Code/Project 7/blog.php
@@ -0,0 +1,10 @@
+
+<?php include "header.html"; ?>
+
+
+<div class="contentBlog">
+  <?php include "blocks.html"; ?>
+  <?php include "slider.html"; ?>
+</div>
+
+<?php include "footer.html"; ?>
---
+++ Other-Code/Project 7/index.php
@@ -0,0 +1,76 @@
+
+<?php include "header.html"; ?>
+
+      <div id="slider">
+      <div id="slider--box">
+         <ul>
+           <li id="img_1"> </li>
+           <li id="img_2"> </li>
+           <li id="img_3"> </li>
+           <li id="img_4"> </li>
+           <li id="img_5"> </li>
+          </ul>
+       </div>
+  <!-- slider box -->
+     <div id="nav--slider">
+        <div id="left--nav"> </div>
+        <div id="right--nav"> </div>
+     </div>
+  </div>
+  <!-- slider -->
+
+  <div class="content">
+    <div class="wrapper">
+    <div class="content-quotes">
+      <div class="Quotes post1">
+        <ul>
+          <li class="image"> </li>
+        </ul>
+        <h1 class="content--box__title" id="m-t"> <q>I`ve traveled in space for 20 years. I`m exhausted.</q> </h1>
+        <p class="content--box__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+      <div class="Quotes post1">
+        <ul>
+          <li class="image Mars"> </li>
+        </ul>
+        <h1 class="content--box__title"> <q>I stuck on Mars! Help me!</q></h1>
+        <p class="content--box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+      <div class="Quotes post1">
+        <ul>
+          <li class="image Bryci"> </li>
+        </ul>
+        <h1 class="content--box__title"> <q>I saved Earth about 10 years ago..</q> </h1>
+        <p class="content--box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+    </div>
+      <div class="content--aboutSpace">
+        <h1 class="content--aboutSpace__title"> About Space </h1>
+        <p class="content--aboutSpace__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        <p class="content--aboutSpace__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        <h1 class="content--aboutSpace__title"> You can buy a ticket to: </h1>
+        <div class="content--aboutSpace">
+          <div class="content--aboutSpace__box">
+            <img class="content-aboutSpace__image" src="image/earth.png">
+            <h2 class="aboutSpace__title"> Mars </h2>
+            <p class="content--aboutSpace__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <a href="#" class="aboutSpace__button"> 5 682$ </a>
+          </div>
+          <div class="content--aboutSpace__box">
+            <img class="content-aboutSpace__image" src="image/earth.png">
+            <h2 class="aboutSpace__title"> Jupiter </h2>
+            <p class="content--aboutSpace__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <a href="#" class="aboutSpace__button"> 2 200$ </a>
+          </div>
+          <div class="content--aboutSpace__box">
+            <img class="content-aboutSpace__image" src="image/earth.png">
+            <h2 class="aboutSpace__title"> Venus </h2>
+            <p class="content--aboutSpace__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <a href="#" class="aboutSpace__button"> 4 912$ </a>
+          </div>
+        </div>
+      </div>
+
+      <?php include "chart.html"; ?>
+
+  <?php include "footer.html" ?>
