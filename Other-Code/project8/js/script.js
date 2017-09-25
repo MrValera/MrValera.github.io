d07c4feadd87963ffe35dc5ddbe9411e973324bf
MrValera <ElternJack@yandex.ua>
2017-09-25 11:51:03 +0300

    Add files via upload

---
+++ Other-Code/project8/js/script.js
@@ -0,0 +1,9 @@
+//HEADER
+$('.header--nav__list').addClass('animated slideInDown');
+$('.main--echo__text').addClass('animated fadeIn');
+$('.echo--button__r-text').addClass('animated fadeIn');
+$('.header--support').addClass('animated fadeInRight');
+
+//SLIDER
+$('.anim').addClass('animated slideInRight');
+$('.slideOne--box').addClass('animated fadeInUp');
