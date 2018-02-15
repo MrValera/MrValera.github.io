ef556d05724c2a8bd960332e3542487e6f450300
MrValera <ElternJack@yandex.ua>
2018-02-15 23:07:15 +0200

    Add files via upload

---
+++ Other-Code/project11/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+	<title>Take App</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<script src="js/jquery-1.9.1.js"></script>
+</head>
+<body>
+<section id="main--header">
+	<p id="name-sites"><span id="name--site__color">PERFECT</span> TAKPAPP.COM</p>
+	<div class="wrapper">
+		<div class="title--page">
+			<img id="logo" src="img/logo.png" alt="take app">
+			<p id="title-logo">take</p>
+		</div>
+		<div class="main--title">
+			<h1>Picture perfect</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+	</div>
+</section>
+<section class="section--padding">
+	<div class="iphone"></div>
+	<div class="wrapper">
+		<div class="text--block text--left">
+			<h1>Start something new</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+
+			<p>eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</p>
+		</div>
+		<div class="text--block text--left fix-position">
+			<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+
+			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+		</div>
+	</div>
+</section>
+<section id="grid--position">
+	<div class="grid">
+		<div class="pink--block block--padding _2anim">
+			<h2 class="grid--block__title">Design quality</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Find Out more →</b></p>
+		</div>
+		<div class="fiolet--block block--padding _2anim">
+			<h2 class="grid--block__title">Built to last</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Find Out more →</b></p>
+		</div>
+		<div class="third--block block--padding _2anim">
+			<h2 class="grid--block__title">Our clients</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Get insights →</b></p>
+			<div class="like"></div>
+		</div>
+		<div class="purpur--block block--padding _2anim">
+			<h2 class="grid--block__title">How to shoo grate</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Find Out more →</b></p>
+		</div>
+		<div class="large--block block--padding _2anim">
+			<h2 class="grid--block__title">Sign up & see why</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Get insights →</b></p>
+			<div class="like"></div>
+		</div>
+		<div class="green--block block--padding _2anim">
+			<h2 class="grid--block__title">Perfect picture</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Find Out more →</b></p>
+		</div>
+		<div class="last--block block--padding _2anim">
+			<h2 class="grid--block__title">Start something new</h2>
+			<p class="grid--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.</p>
+			<p class="grid--block__text"><b>Find Out more →</b></p>
+		</div>
+	</div>
+</section>
+<section id="section-end">
+	<div class="wrapper">
+		<h1 class="column--main__title">A look at the details</h1>
+		<div class="column--block">
+			<h2 class="column--block__title">Web based</h2>
+			<p class="column--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+		<div class="column--block m-l">
+			<h2 class="column--block__title">Performence</h2>
+			<p class="column--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+		<div class="column--block m-l">
+			<h2 class="column--block__title">Clultral</h2>
+			<p class="column--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+		<div class="column--block m-l">
+			<h2 class="column--block__title">Sustainability</h2>
+			<p class="column--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+	</div>
+</section>
+<section id="second-bg">
+	<div class="wrapper">
+		<div class="info--block">
+			<h1 class="info--block__title">More power <br> behind every pixel.</h1>
+			<p class="info--block__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p class="grid--block__text"><b>Set the options →</b></p>
+		</div>
+	</div>
+</section>
+<section id="green--block">
+	<div class="wrapper">
+		<h1 class="column--main__title">A look at the details</h1>
+		<div class="column--block__green">
+			<h2 class="column--block__title-g">Web based</h2>
+			<p class="column--block__text-g">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+		<div class="column--block__green m-l">
+			<h2 class="column--block__title-g">Team login</h2>
+			<p class="column--block__text-g">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+		<div class="column--block__green m-l">
+			<h2 class="column--block__title-g">Great Support</h2>
+			<p class="column--block__text-g">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+	</div>
+</section>
+<section id="buy--block">
+	<div class="wrapper">
+		<div class="get--solo _3anim">
+			<p class="tag">LEGACY</p>
+			<h1 class="buy--title">Go it solo</h1>
+			<p class="buy--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+			<button id="buy--button">BUY NOW $9</button><br>
+			<img class="buy--platform" src="img/fa-apple.png" alt="apple">
+			<img class="buy--platform" src="img/fa-windows.png" alt="windows">
+		</div>
+		<div class="get--team _3anim">
+			<p class="tag tag--purpur">MOST POPOLAR</p>
+			<h1 class="buy--title">Team Plan</h1>
+			<p class="buy--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+			<button id="buy--button">BUY NOW $9</button><br>
+			<img class="buy--platform" src="img/fa-apple.png" alt="apple">
+			<img class="buy--platform" src="img/fa-windows.png" alt="windows">
+		</div>
+	</div>
+</section>
+<footer id="footer-bg">
+	<div class="wrapper">
+		<div class="footer--left_box">
+			<h3>Get in touch</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+		</div>
+		<div class="footer--right_box">
+			<h3>Stay in touch</h3>
+			<div class="footer--input_box">
+				<label>
+					<input id="input-box" type="email">
+					<button id="send"></button>
+				</label>
+			</div>
+			<small id="note">We don`t send spam. Actually, who are we kiding, we`ll spam <br> the shit out of you`r inbox</small>
+		</div>
+		<div class="footer--column">
+			<div class="f-column_box">
+				<h4>Resources</h4>
+				<ul>
+					<a href="#"><li>Tour</li></a>
+					<a href="#"><li>Customers</li></a>
+					<a href="#"><li>Pricing and Plans</li></a>
+					<a href="#"><li>New Features</li></a>
+					<a href="#"><li>Education</li></a>
+				</ul>
+			</div>
+			<div class="f-column_box f-padding">
+				<h4>Features</h4>
+				<ul>
+					<a href="#"><li>Tour</li></a>
+					<a href="#"><li>Customers</li></a>
+					<a href="#"><li>Pricing and Plans</li></a>
+					<a href="#"><li>New Features</li></a>
+					<a href="#"><li>Education</li></a>
+				</ul>
+			</div>
+			<div class="f-column_box f-padding">
+				<h4>How To`s</h4>
+				<ul>
+					<a href="#"><li>Tour</li></a>
+					<a href="#"><li>Customers</li></a>
+					<a href="#"><li>Pricing and Plans</li></a>
+					<a href="#"><li>New Features</li></a>
+					<a href="#"><li>Education</li></a>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<script type="text/javascript">
+	$(".like").click(function(e) {
+	  e.preventDefault();
+	  $(this).addClass('active');
+	})	
+</script>
+<script src="js/script.js"></script>
+</body>
+</html>
