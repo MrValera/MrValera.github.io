750b47ee81cd0c9a60e6809ad55a213c4775548f
MrValera <ElternJack@yandex.ua>
2018-07-11 14:36:37 +0300

    Add files via upload

---
+++ Other-Code/project12/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog</title>
+	<link rel="stylesheet/less" href="less/style.less">
+	<script src="js/jquery-3.0.1.js"></script>
+	<script src="js/less.min.js"></script>
+</head>
+<body>
+	<header id="header">
+		<div class="wrapper">
+			<div class="header-name">
+				<h1 class="name-site">Tourism.</h1>
+			</div>
+			<div class="header-block">
+				<h2>EXPLORE THE</h2>
+				<h1>DEEP FOREST</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+			</div>
+		</div>
+	</header>
+	<div class="gallery">
+		<div class="wrapper">
+			<div class="gallery-title">
+				<h1>Gallery</h1>
+				<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+			</div>
+			<div class="gallery-box">
+				<div class="block_1"></div>
+				<div class="block_2"></div>
+				<div class="block_3"></div>
+				<div class="block_4"></div>
+				<div class="block_5"></div>
+				<div class="block_6"></div>
+			</div>
+			<div class="side-img"></div>
+		</div>
+	</div>
+	<div class="banner">
+		<div class="ban-wrapper">
+			<h2>Lorem Ipsum</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+		</div>
+	</div>
+	<div class="stat">
+		<div class="wrapper">
+			<h1>Statistics</h1>
+			<h2>Did You Know?</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<div class="stat-line">
+				<ul>
+					<li class="stat-line_col">
+						<p class="bold-col">2000</p>
+						<p>Lorem Ipsum</p>
+					</li>
+					<li class="stat-line_col">
+						<p class="bold-col">23</p>
+						<p>Lorem Ipsum</p>
+					</li>
+					<li class="stat-line_col">
+						<p class="bold-col">19382</p>
+						<p>Lorem Ipsum</p>
+					</li>
+					<li class="stat-line_col">
+						<p class="bold-col">120</p>
+						<p>Lorem Ipsum</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<footer id="footer">
+		<div class="wrapper">
+			<div class="ban-wrapper">
+				<h2>Lorem Ipsum</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+				<iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/kb2-6zemmj8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<h1 class="name-site">Tourism.</h1>
+		</div>
+		</div>
+	</footer>
+</body>
+</html>
